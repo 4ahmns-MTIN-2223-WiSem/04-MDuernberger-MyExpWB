5a74a2ab7e425a52a24b9b6dc26a9cb4481b8e70
mduernberger <melanie.duernberger05@htl-salzburg.ac.at>
2022-11-24 15:10:26 +0100

    added collider

---
+++ Assets/MyGame/Scripts/CollisionScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    public GameObject daube;
+    public GameObject eisstock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "daube")
+        {
+            Debug.Log("Boom");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
